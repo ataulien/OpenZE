42a99567dc2d211cf85faabef27efba613146833
Andre Taulien <a.taulien@live.de>
2016-02-11 22:19:57 +0100

    nicer test scene

--- src/engine/objectfactory.cpp
+++ src/engine/objectfactory.cpp
@@ -69,25 +69,32 @@ uint32_t Engine::ObjectFactory::createObject()
     m_Collision[entity].pRigidBody->setFriction(0.8f);
 
 #ifdef ZE_GAME
-    for(int i = 0; i < 100; i++)
+	RAPI::RPixelShader* ps = RAPI::REngine::ResourceCache->GetCachedObject<RAPI::RPixelShader>("simplePS");
+	RAPI::RVertexShader* vs = RAPI::REngine::ResourceCache->GetCachedObject<RAPI::RVertexShader>("simpleVS");
+	RAPI::RStateMachine& sm = RAPI::REngine::RenderingDevice->GetStateMachine();
+	RAPI::RBuffer *b = MakeBox(1.0f);
+
+	RAPI::RInputLayout* inputLayout = RAPI::RTools::CreateInputLayoutFor<Renderer::SimpleVertex>(vs);
+
+	const int n = 100;
+	const float turns = 5;
+	const float radius = 4.0f;
+	const float heightmod = 0.7f;
+    for(int i = 0; i < n; i++)
     {
+		float p = (static_cast<float>(i) / static_cast<float>(n)) * R_PI * turns;
+		btVector3 s = btVector3(sinf(p) * radius, 50.0f + static_cast<float>(i) * heightmod, cosf(p) * radius);
+
         entity = createEntity();
         m_Mask[entity] = static_cast<EComponents>(C_COLLISION | C_VISUAL);
         m_Collision[entity].pCollisionShape = new btBoxShape(btVector3(1.0f, 1.0f, 1.0f));
-        m_Collision[entity].pMotionState = new btDefaultMotionState(btTransform(btQuaternion(0.0f, 0.0f, 0.0f, 1.0f), btVector3(0.0f + static_cast<float>(i), 50.0f + static_cast<float>(i), 0.0f)));
+        m_Collision[entity].pMotionState = new btDefaultMotionState(btTransform(btQuaternion(0.0f, 0.0f, 0.0f, 1.0f), s));
         btVector3 inertia;
         m_Collision[entity].pCollisionShape->calculateLocalInertia(1.0f, inertia);
         m_Collision[entity].pRigidBody = new btRigidBody(1.0f, m_Collision[entity].pMotionState,
                                                          m_Collision[entity].pCollisionShape, inertia);
         m_Collision[entity].pRigidBody->setRestitution(0.2f);
         m_pEngine->physicsSystem()->addRigidBody(m_Collision[entity].pRigidBody);
-
-        RAPI::RPixelShader* ps = RAPI::REngine::ResourceCache->GetCachedObject<RAPI::RPixelShader>("simplePS");
-        RAPI::RVertexShader* vs = RAPI::REngine::ResourceCache->GetCachedObject<RAPI::RVertexShader>("simpleVS");
-        RAPI::RStateMachine& sm = RAPI::REngine::RenderingDevice->GetStateMachine();
-        RAPI::RBuffer *b = MakeBox(1.0f);
-
-        RAPI::RInputLayout* inputLayout = RAPI::RTools::CreateInputLayoutFor<Renderer::SimpleVertex>(vs);
 
         m_Visual[entity].pObjectBuffer = RAPI::REngine::ResourceCache->CreateResource<RAPI::RBuffer>();
         m_Visual[entity].pObjectBuffer->Init(nullptr, sizeof(Math::Matrix), sizeof(Math::Matrix), RAPI::EBindFlags::B_CONSTANTBUFFER, RAPI::EUsageFlags::U_DYNAMIC, RAPI::ECPUAccessFlags::CA_WRITE);
